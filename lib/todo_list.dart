b1940cf2970df18fc2aaa4d2a11027357a7e4c5d
kahoragachau <joelkahoragachau6@gmail.com>
2024-06-04 12:28:45 +0300

    feat: Add Import of todo model

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/models/todo.dart';
 
 class TodoList extends StatefulWidget {
-  const TodoList({super.key});
+  const TodoList({required this.todos, super.key});
+
+  final List<Todo> todos;
 
   @override
   State<TodoList> createState() => _TodoListState();
@@ -10,6 +13,19 @@ class TodoList extends StatefulWidget {
 class _TodoListState extends State<TodoList> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.builder(
+      itemCount: widget.todos.length,
+      itemBuilder: (_, index){
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 6),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              color: widget.todos[index.prio]
+            ),
+          ),
+        )
+      },
+    );
   }
 }
