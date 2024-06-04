eb8cb15eeaed88fbcf070eb361017cb56c2ac435
kahoragachau <joelkahoragachau6@gmail.com>
2024-06-04 13:20:11 +0300

    feat: Add dummy data for todo

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/models/todo.dart';
+import 'package:todo_list/todo_list.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,6 +10,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final List<Todo> todos = [
+    const Todo(
+      title: 'Buy Milk',
+      description: 'There is no milk left in the fridge',
+      priority: Priority.high,
+    ),
+
+    const Todo(
+      title: 'Make Bed', 
+      description: 'Keep things Tidy', 
+      priority: Priority.low
+    ),
+
+    const Todo(
+      title: 'Pay Bills', 
+      description: 'The Net Bill is due', 
+      priority: Priority.urgent
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
